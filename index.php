e35f0998cd422c07e66d54453ef1d76d1d6cb2bf
Jay <staticrunescape@gmail.com>
2015-10-04 23:36:33 +0100

    Basic layout
    Slight basic layout done.

---
+++ index.php
@@ -0,0 +1,30 @@
+<head>
+  <script src="bootstrap/jquery/jquery-2.1.4.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style/sheet.css">
+  <script src="javascript/script.js"></script>
+  <title>MatchWinna</title>
+  <link rel="shortcut icon" href="images/favicon.ico">
+</head>
+
+<body>
+  <div id='login_bar'>
+    <img src='images/logo.png' style='max-height:75px; width:auto;'>
+    <div id='nav_bar'>
+      <a href='/football'><div id='nav_option'>Home</div></a>
+      <a href='#'><div id='nav_option'>Log-in</div></a>
+      <a href='#'><div id='nav_option'>Sign-up</div></a>
+    </div>
+  </div>
+
+    <div id='main'>
+    <div class='col-sm-12' id='match'>
+      <div id='badge1'><img src='images/chelsea.png'></div>
+      <div id='club_text'>Chelsea</div>
+      <div id='versus'>Vs</div>
+      <div id='club_text2'>Southampton</div>
+      <div id='badge2'><img src='images/badge2.png'></div>
+    </div>
+  </div>
+</body>
